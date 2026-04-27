44f220e698201b2587eb1bc636bd245c3cb761b8
MarkRockSoul <mark.antony1589@gmail.com>
2026-04-26 19:13:41 -0500

    feat: agrego clase CuentaBancaria

---
+++ CuentaBancaria.java
@@ -0,0 +1,18 @@
+package Clases;
+
+public class CuentaBancaria {
+    private double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double _saldo) {
+        if (_saldo >= 0) {
+            this.saldo = _saldo;
+        } else {
+            System.out.println("El saldo no puede ser negativo.");
+        }
+    }
+
+}
